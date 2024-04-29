b362517a89c35a81166710a16f5d548463df444b
nagasai <avuthunagasaireddy@gmail.com>
2024-04-29 20:42:57 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
-
+source common.sh
 echo -e "\e[33m Setup the MongoDB repo file \e[0m"
 cp /root/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[31m Installing mongodb \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
-
+stat_check $?
 
 
 echo -e "\e[31m Start & Enable MongoDB Service \e[0m"
@@ -13,6 +13,9 @@ systemctl start mongod &>>/tmp/roboshop.log
 
 echo -e "\e[31m updating mongodb listen address \e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat_check $?
 
 echo -e "\e[31m Start & Enable MongoDB Service \e[0m"
 systemctl restart mongod &>>/tmp/roboshop.log
+
+stat_check $?
--- mysql.sh
+++ mysql.sh
@@ -1,13 +1,14 @@
+source common.sh
 echo -e "\e[33m enabling disable MySQL 8 version \e[0m"
 dnf module disable mysql -y &>>/tmp/roboshop.log
 
-
+stat_check $?
 echo -e "\e[33m enabling disable MySQL 8 version \e[0m"
 cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m Setup the MySQL5.7 repo file \e[0m"
 dnf install mysql-community-server -y &>>/tmp/roboshop.log
-
+ stat_check $?
 echo -e "\e[33m Start MySQL Service \e[0m"
 systemctl enable mysqld &>>/tmp/roboshop.log
 
@@ -17,3 +18,4 @@ echo -e "\e[33m Start change the default root password  \e[0m"
 mysql_secure_installation --set-root-pass $1
 echo -e "\e[33m check the new password working or not  \e[0m"
 mysql -uroot -pRoboShop@1
+ stat_check $?
