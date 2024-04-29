39751bf6b19718a5aaa79e5bec027a8162edb283
nagasai <avuthunagasaireddy@gmail.com>
2024-04-29 15:45:53 +0530

    learning front end automation

--- common.sh
+++ common.sh
@@ -37,3 +37,15 @@ nodejs() {
 
 }
 
+mongo_schema_setup(){
+
+  echo -e "${color} copy mongodb repo file ${nocolor}"
+  cp /root/roboshop-shell-dup/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
+
+  echo -e "${color} install Mongodb server ${nocolor}"
+  dnf install mongodb-org-shell -y &>>$log_file
+
+  echo -e "${color} Loading List of products we want to sell ${nocolor}"
+  mongo --host mongodb-dev.devopsb73.tech <${app_path}/schema/$component.js &>>$log_file
+}
+
