8901e26c9b28b5063f86a4794d5a129df46a8f97
nagasai <avuthunagasaireddy@gmail.com>
2024-04-29 15:37:53 +0530

    learning front end automation

--- catalogue.sh
+++ catalogue.sh
@@ -1,35 +1,7 @@
 source common.sh
 component=catalogue
 
-echo -e "${color} enabling nodejs version 18 ${nocolor}"
-dnf module disable nodejs -y &>>$log_file
-dnf module enable nodejs:18 -y &>>$log_file
-
-
-echo -e "${color} installing nodejs ${nocolor}"
-dnf install nodejs -y &>>$log_file
-echo -e "${color} adding user ${nocolor}"
-useradd roboshop &>>$log_file
-echo -e "${color} create app directory ${nocolor}"
-rm -rf ${app_path}
-mkdir ${app_path}
-
-echo -e "${color} Download the application code to created app directory ${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-cd ${app_path}
-unzip /tmp/$component.zip &>>$log_file
-echo -e "${color} download the dependencies ${nocolor}"
-cd ${app_path}
-npm install &>>$log_file
-echo -e "${color} Setting up SystemD Catalogue Service ${nocolor}"
-cp  /root/roboshop-shell-dup/$component.service /etc/systemd/system/$component.service
-
-
-echo -e "${color} starting catalogue ${nocolor}"
-
-systemctl daemon-reload
-systemctl enable $component &>>$log_file
-systemctl start $component &>>$log_file
+nodejs
 
 echo -e "${color} copy mongodb repo file ${nocolor}"
 cp /root/roboshop-shell-dup/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
--- common.sh
+++ common.sh
@@ -2,3 +2,38 @@ color="\e[36m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+nodejs() {
+
+  echo -e "${color} enabling nodejs version 18 ${nocolor}"
+  dnf module disable nodejs -y &>>log_file
+  dnf module enable nodejs:18 -y &>>log_file
+
+
+  echo -e "${color} installing nodejs ${nocolor}"
+  dnf install nodejs -y &>>log_file
+  echo -e "${color} adding user ${nocolor}"
+  useradd roboshop &>>log_file
+
+  echo -e "${color} create app directory ${nocolor}"
+  rm -rf ${app_path} &>>log_file
+  mkdir ${app_path} &>>log_file
+
+  echo -e "${color} Download the application code to created app directory ${nocolor}"
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>log_file
+  cd ${app_path} &>>log_file
+  unzip /tmp/${component}.zip &>>log_file
+  echo -e "${color} download the dependencies ${nocolor}"
+  cd ${app_path} &>>log_file
+  npm install &>>log_file
+  echo -e "${app_path} Setting up SystemD ${component} Service ${nocolor}"
+  cp  /root/roboshop-shell-dup/${component}.service /etc/systemd/system/${component}.service &>>log_file
+
+  echo -e "${app_path} starting ${component} ${nocolor}"
+
+  systemctl daemon-reload &>>log_file
+  systemctl enable ${component} &>>log_file
+  systemctl start ${component} &>>log_file
+
+
+}
+
