cf14d23799e25f52c41c49d029b70e21c4e21e71
nagasai <avuthunagasaireddy@gmail.com>
2024-04-29 16:23:11 +0530

    learning front end automation

--- common.sh
+++ common.sh
@@ -2,37 +2,54 @@ color="\e[36m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
-nodejs() {
 
-  echo -e "${color} enabling nodejs version 18 ${nocolor}"
-  dnf module disable nodejs -y &>>log_file
-  dnf module enable nodejs:18 -y &>>log_file
+# shellcheck disable=SC1073
+app_presetup() {
 
-
-  echo -e "${color} installing nodejs ${nocolor}"
-  dnf install nodejs -y &>>log_file
   echo -e "${color} adding user ${nocolor}"
   useradd roboshop &>>log_file
 
   echo -e "${color} create app directory ${nocolor}"
   rm -rf ${app_path} &>>log_file
   mkdir ${app_path} &>>log_file
-
   echo -e "${color} Download the application code to created app directory ${nocolor}"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>log_file
   cd ${app_path} &>>log_file
+  echo -e "${color} extract the content ${nocolor}"
   unzip /tmp/${component}.zip &>>log_file
-  echo -e "${color} download the dependencies ${nocolor}"
-  cd ${app_path} &>>log_file
-  npm install &>>log_file
+
+
+}
+
+
+systemd_setup() {
+
+
   echo -e "${color} Setting up SystemD ${component} Service ${nocolor}"
-  cp  /root/roboshop-shell-dup/${component}.service /etc/systemd/system/${component}.service &>>log_file
-
+  cp /root/roboshop-shell-dup/${component}.service /etc/systemd/system/${component}.service &>>$log_file
   echo -e "${color} starting ${component} ${nocolor}"
-
   systemctl daemon-reload &>>log_file
   systemctl enable ${component} &>>log_file
   systemctl start ${component} &>>log_file
+}
+
+
+nodejs() {
+
+  echo -e "${color} enabling nodejs version 18 ${nocolor}"
+  dnf module disable nodejs -y &>>log_file
+  dnf module enable nodejs:18 -y &>>log_file
+
+  echo -e "${color} installing nodejs ${nocolor}"
+  dnf install nodejs -y &>>log_file
+
+  app_presetup
+
+  echo -e "${color} download the dependencies ${nocolor}"
+  cd ${app_path} &>>log_file
+  npm install &>>log_file
+
+  systemd_setup
 
 
 }
@@ -49,3 +66,33 @@ mongo_schema_setup(){
   mongo --host mongodb-dev.devopsb73.tech <${app_path}/schema/$component.js &>>$log_file
 }
 
+
+mysql_schema_setup() {
+
+  echo -e "${color} installing mysql ${nocolor}"
+  dnf install mysql -y &>>$log_file
+  echo -e "${color} Loading Schema ${nocolor}"
+  mysql -h mysql-dev.devopsb73.tech -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql &>>$log_file
+
+
+}
+
+maven() {
+  echo -e "${color} installing maven ${nocolor}"
+
+  dnf install maven -y &>>$log_file
+
+  app_presetup
+
+  echo -e "${color} download the dependencies & build the application ${nocolor}"
+  cd ${app_path}
+  mvn clean package &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+
+  mysql_schema_setup
+
+  systemd_setup
+
+
+
+}
