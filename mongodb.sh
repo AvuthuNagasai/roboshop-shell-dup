6a39f4fa1fa3a1941f38dad037feb7ff4e432381
nagasai <avuthunagasaireddy@gmail.com>
2024-04-30 20:30:18 +0530

    learning front end automation

--- mongodb.sh
+++ mongodb.sh
@@ -1,21 +1,25 @@
 source common.sh
-echo -e "\e[33m Setup the MongoDB repo file \e[0m"
-cp /root/roboshop-shell-dup/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+color="\e[36m"
+nocolor="\e[0m"
+log_file="/tmp/roboshop.log"
+app_path="/app"
+echo -e "${color} Setup the MongoDB repo file ${nocolor}"
+cp /root/roboshop-shell-dup/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
-echo -e "\e[31m Installing mongodb \e[0m"
+echo -e "${color} Installing mongodb \e[0m"
 dnf install mongodb-org -y &>>/tmp/roboshop.log
 stat_check $?
 
 
-echo -e "\e[31m Start & Enable MongoDB Service \e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
-systemctl start mongod &>>/tmp/roboshop.log
+echo -e "${color} Start & Enable MongoDB Service ${nocolor}"
+systemctl enable mongod &>>$log_file
+systemctl start mongod &>>$log_file
 
-echo -e "\e[31m updating mongodb listen address \e[0m"
+echo -e "${color} updating mongodb listen address ${nocolor}"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 stat_check $?
 
-echo -e "\e[31m Start & Enable MongoDB Service \e[0m"
+echo -e "${color} Start & Enable MongoDB Service ${nocolor}"
 systemctl restart mongod &>>/tmp/roboshop.log
 
 stat_check $?
